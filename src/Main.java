aa771c2a0e3a9cbf944a69795c8ddd37bce05787
Robert Sun <145725240+WashEyesWithTide@users.noreply.github.com>
2026-03-12 21:26:18 -0700

    done

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,19 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
+    private static List<Student> students;
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        students = new ArrayList<Student>();
+        students.add(new Student(398639, "Alice", 3.6));
+        students.add(new Student(774789, "Bob", 3.9));
+        students.add(new Student(678392, "Charlie", 3.7));
+        students.add(new Student(693856, "David", 4.0));
+        students.add(new Student(583920, "Eve", 3.8));
+        students.add(new Student(483920, "Frank", 3.5));
+        students.sort(null);
+        for (Student student : students) {
+            System.out.println(student.toString());
+        }
     }
 }
